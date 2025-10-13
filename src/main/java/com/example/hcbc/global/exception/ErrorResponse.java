6b847ff88e4b1f34dae15f7d34a4ed32b1c6197f
박하민 <hamin090403@gmail.com>
2025-10-13 17:47:21 +0900

    feat(exception): add ErrorResponse

---
+++ src/main/java/com/example/hcbc/global/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.hcbc.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
