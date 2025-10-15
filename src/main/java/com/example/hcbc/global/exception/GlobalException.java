8762aebb3fbe8f11d96f03f909ea78a7f158cf70
hej090224 <fc49854985@gmail.com>
2025-10-15 17:52:56 +0900

    feat: add GlobalException handler

---
+++ src/main/java/com/example/hcbc/global/exception/GlobalException.java
@@ -0,0 +1,11 @@
+package com.example.hcbc.global.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GlobalException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
