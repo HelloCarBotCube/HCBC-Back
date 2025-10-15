b2c6f845010bb37482c5c4e42aa46f70f7f7c6bd
hej090224 <fc49854985@gmail.com>
2025-10-15 17:52:45 +0900

    feat: add ErrorCode enum

---
+++ src/main/java/com/example/hcbc/global/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.hcbc.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    //auth
+    DUPLICATE_LOGIN_ID(409, "이미 존재하는 아이디입니다."),
+    NOT_FOUND_USER(401, "존재하지 않는 사용자입니다."),
+    UNAUTHORIZED(401, "아이디 또는 비밀번호가 잘못되었습니다."),
+
+    //user
+    NOT_FOUND_USER_DETAIL(404, "회원 상세 정보를 찾을 수 없습니다.");
+
+    private final int status;
+    private final String message;
+}
