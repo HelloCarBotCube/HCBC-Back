fe5546be3b126ebcf903cb07b8b135417706e9bb
박하민 <hamin090403@gmail.com>
2025-10-16 10:00:26 +0900

    update(exception): INVALID_USER_PRINCIPAL 추가

--- src/main/java/com/example/hcbc/global/exception/ErrorCode.java
+++ src/main/java/com/example/hcbc/global/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     UNAUTHORIZED(401, "아이디 또는 비밀번호가 잘못되었습니다."),
 
     //user
-    NOT_FOUND_USER_DETAIL(404, "회원 상세 정보를 찾을 수 없습니다.");
+    NOT_FOUND_USER_DETAIL(404, "회원 상세 정보를 찾을 수 없습니다."),
+    INVALID_USER_PRINCIPAL(401, "현재 인증된 사용자의 정보가 유효하지 않습니다.");
 
     private final int status;
     private final String message;
