dcdc0ac57c5fc3cb192cdbe816c34233dbe9c92e
박하민 <hamin090403@gmail.com>
2025-10-16 10:02:46 +0900

    update(user): update controller

--- src/main/java/com/example/hcbc/domain/user/controller/UserController.java
+++ src/main/java/com/example/hcbc/domain/user/controller/UserController.java
@@ -2,35 +2,35 @@ package com.example.hcbc.domain.user.controller;
 
 import com.example.hcbc.domain.user.dto.ProfileResponseDto;
 import com.example.hcbc.domain.user.dto.UpdateMyProfileRequestDto;
-import com.example.hcbc.domain.user.service.UserProfileService;
+import com.example.hcbc.domain.user.service.GetMyProfileService;
+import com.example.hcbc.domain.user.service.GetOtherProfileService;
+import com.example.hcbc.domain.user.service.UpdateMyProfileService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
 
-    private final UserProfileService userProfileService;
+    private final GetOtherProfileService  getOtherProfileService;
+    private final GetMyProfileService getMyProfileService;
+    private final UpdateMyProfileService updateMyProfileService;
 
-    @GetMapping("/user")
-    public ResponseEntity<ProfileResponseDto> getOtherProfile(@RequestParam("userId") Long targetUserId) {
-        return ResponseEntity.ok(userProfileService.getOtherProfile(targetUserId));
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfileResponseDto> getOtherProfile(@PathVariable("id") Long userId) {
+        return ResponseEntity.ok(getOtherProfileService.execute(userId));
     }
 
     @GetMapping("/myprofile")
-    public ResponseEntity<ProfileResponseDto> getMyProfile(
-            @RequestHeader("X-User-Id") Long currentUserId
-    ) {
-        return ResponseEntity.ok(userProfileService.getMyProfile(currentUserId));
+    public ResponseEntity<ProfileResponseDto> getMyProfile() {
+        return ResponseEntity.ok(getMyProfileService.execute());
     }
 
-    @PatchMapping("/myprofile/update")
-    public ResponseEntity<ProfileResponseDto> updateMyProfile(
-            @RequestHeader("X-User-Id") Long currentUserId,
-            @RequestBody UpdateMyProfileRequestDto request
-    ) {
-        return ResponseEntity.ok(userProfileService.updateMyProfile(currentUserId, request));
+    @PatchMapping("/myprofile-update")
+    public ResponseEntity<ProfileResponseDto> updateMyProfile(@RequestBody @Valid UpdateMyProfileRequestDto request) {
+        return ResponseEntity.ok(updateMyProfileService.execute(request));
     }
 }
