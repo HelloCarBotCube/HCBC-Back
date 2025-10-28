09e4519acb601db5014d3fa2b0fcd323b9d84ca6
박하민 <hamin090403@gmail.com>
2025-10-28 23:01:13 +0900

    feat: add ChatMessageResponse

---
+++ src/main/java/com/example/hcbc/domain/chat/dto/response/ChatMessageResponse.java
@@ -0,0 +1,8 @@
+package com.example.hcbc.domain.chat.dto.response;
+
+public record ChatMessageResponse(
+        String roomId,
+        Long senderId,
+        String name,
+        String content
+) {}
