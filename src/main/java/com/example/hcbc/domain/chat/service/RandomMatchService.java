c5c4d802c0677805d5b734c7337c0e287f208cd3
박하민 <hamin090403@gmail.com>
2025-10-28 23:11:47 +0900

    feat: add RandomMatchService

---
+++ src/main/java/com/example/hcbc/domain/chat/service/RandomMatchService.java
@@ -0,0 +1,7 @@
+package com.example.hcbc.domain.chat.service;
+
+import com.example.hcbc.domain.chat.dto.result.RandomMatchResult;
+
+public interface RandomMatchService {
+    RandomMatchResult execute();
+}
