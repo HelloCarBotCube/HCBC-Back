470ec5d50d62a32f317066a98af10dc2a7954398
박하민 <hamin090403@gmail.com>
2025-10-28 23:13:21 +0900

    feat: add RoomMember

---
+++ src/main/java/com/example/hcbc/domain/chat/entity/RoomMember.java
@@ -0,0 +1,25 @@
+package com.example.hcbc.domain.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String roomId;
+
+    @Column(nullable = false)
+    private Long userId;
+}
