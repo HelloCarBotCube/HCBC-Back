08a670515546a7af85846bcc867c63de9c20c70b
박하민 <hamin090403@gmail.com>
2025-10-28 23:02:07 +0900

    feat: add ChatRoom

---
+++ src/main/java/com/example/hcbc/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,21 @@
+package com.example.hcbc.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
