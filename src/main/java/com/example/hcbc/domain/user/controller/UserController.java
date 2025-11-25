d14dcb6d98a9314a1c2cbde2488f889622a295e2
박하민 <hamin090403@gmail.com>
2025-11-25 19:34:13 +0900

    update: swagger 추가

--- src/main/java/com/example/hcbc/domain/user/controller/UserController.java
+++ src/main/java/com/example/hcbc/domain/user/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.hcbc.domain.user.dto.UpdateMyProfileRequestDto;
 import com.example.hcbc.domain.user.service.GetMyProfileService;
 import com.example.hcbc.domain.user.service.GetOtherProfileService;
 import com.example.hcbc.domain.user.service.UpdateMyProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@Tag(name = "User API", description = "User API입니다")
 public class UserController {
 
     private final GetOtherProfileService  getOtherProfileService;
@@ -20,16 +23,22 @@ public class UserController {
     private final UpdateMyProfileService updateMyProfileService;
 
     @GetMapping("/{id}")
+    @Tag(name = "User API")
+    @Operation(summary = "getOtherProfile", description = "상대 프로필 조회 API입니다.")
     public ResponseEntity<ProfileResponseDto> getOtherProfile(@PathVariable("id") Long userId) {
         return ResponseEntity.ok(getOtherProfileService.execute(userId));
     }
 
     @GetMapping("/myprofile")
+    @Tag(name = "User API")
+    @Operation(summary = "getMyProfile", description = "내 프로필 조회 API입니다.")
     public ResponseEntity<ProfileResponseDto> getMyProfile() {
         return ResponseEntity.ok(getMyProfileService.execute());
     }
 
     @PatchMapping("/myprofile-update")
+    @Tag(name = "User API")
+    @Operation(summary = "updateMyProfile", description = "내 프로필 수정 API입니다.")
     public ResponseEntity<ProfileResponseDto> updateMyProfile(@RequestBody @Valid UpdateMyProfileRequestDto request) {
         return ResponseEntity.ok(updateMyProfileService.execute(request));
     }
